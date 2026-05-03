ae2385e04fe708928908fb9dcc64f8e39c5e4fbc
aleezamo <aleezamohsinrizvi001@gmail.com>
2026-05-02 22:54:22 -0400

    added input validation to prevent internal server errors

--- app/main.py
+++ app/main.py
@@ -3,20 +3,26 @@ from sqlalchemy.orm import Session
 
 from .database import Base, engine, get_db
 from .schema import AIEvent
+from pydantic import BaseModel, Field
 
 app = FastAPI()
+
+class TrackEventRequest(BaseModel):
+    hostName: str = Field(..., min_length=4)
+    aiSource: str = Field(..., min_length=5)
+    pathName: str = Field(..., min_length=1)
 
 @app.get("/healthcheck")
 def health():
     return {"status": "ok"}
 
 @app.post("/track")
-def track_event(payload: dict, db: Session = Depends(get_db)):
+def track_event(payload: TrackEventRequest, db: Session = Depends(get_db)):
 
     event = AIEvent(
-        host_name=payload.get("hostName"),
-        ai_source=payload.get("aiSource"),
-        path_name=payload.get("pathName"),
+        host_name=payload.hostName,
+        ai_source=payload.aiSource,
+        path_name=payload.pathName,
     )
 
     db.add(event)
