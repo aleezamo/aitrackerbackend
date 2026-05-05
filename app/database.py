9d084290587d6fdadcc0001d49d27df01f68b43e
aleezamo <aleezamohsinrizvi001@gmail.com>
2026-05-04 23:02:53 -0400

    updated database.py to read indiviual environment variables from .env and added .env.example

--- app/database.py
+++ app/database.py
@@ -6,6 +6,13 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
+DB_PORT = os.getenv("DB_PORT")
+DB_USERNAME = os.getenv("DB_USERNAME")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+
+DATABASE_URL = f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 
