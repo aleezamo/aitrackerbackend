3edb5e83a711659794daac35698cb3c7d9fdbed6
aleezamo <aleezamohsinrizvi001@gmail.com>
2026-05-09 23:02:04 -0400

    started node.js and express web app for the dashboard

---
+++ dashboard/server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 8000
+
+app.set("view engine", "ejs")
+
+app.get('/', (req, res) => {
+  res.render("dashboard", {title: "My Dashboard"})
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
