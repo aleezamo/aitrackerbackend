18fbdbdc5ed8f57f5a35170506ecfa78e1898e17
aleezamo <aleezamohsinrizvi001@gmail.com>
2026-05-14 00:39:12 -0400

    started building dashboard

--- dashboard/server.js
+++ dashboard/server.js
@@ -1,12 +1,23 @@
 const express = require('express')
+const pool = require('./db');
+
 const app = express()
 const port = 8000
 
 app.set("view engine", "ejs")
 
-app.get('/', (req, res) => {
-  res.render("dashboard", {title: "My Dashboard"})
-})
+app.get('/', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT ai_source, COUNT(*) AS count from ai_traffic_events GROUP BY ai_source ORDER BY count DESC');
+    console.log(result.rows); 
+    //res.send("Heloooo")   
+    res.render("dashboard", {title: "Dashboard", aiSources: result.rows});
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('DB ERROR');
+  }
+  
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
